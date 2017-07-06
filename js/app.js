29429838f410599f4bae9c8988f3a052ec0e564e
Lefort <jordan.lefort@laposte.net>
2017-07-06 15:19:49 +0200

    correction jquery

--- js/app.js
+++ js/app.js
@@ -1,52 +1,20 @@
-// Exercice one
+// Exercice 1
+var addText = $('#addText');
+addText.html('Hello world');
+
+//Exercice 2
+$('#remove').remove();
+
+//Exercice 3
 
 //method 1
-var paragraph = document.getElementById('addText');
-paragraph.textContent = "Hello world";
-
-// method 3
-var paragraph = document.getElementById('addText');
-paragraph.innerHTML = "Hello world";
-
-
-// Exercice 2
+$('#changeColor').css('backgroundColor','red');
 
 //method 2
-var removeId = document.getElementById('remove');
-removeId.remove();
-
-// Exercice 3
-
-//method 1
-var changeColor = document.getElementById('changeColor');
-changeColor.style.backgroundColor = 'red';
-
-//method 2
-var changeColor = document.getElementById('changeColor');
-changeColor.classList.remove('changeColor');
-changeColor.classList.add('backgroundRed');
+//$('#changeColor').addClass('background');
 
 //Exercice 4
 
-//method 1
-
-var addChildren = document.getElementById('addChildren');
-var child;
-
 for (var i = 0; i < 4; i++) {
-  child = document.createElement('p');
-  console.log(child);
-  addChildren.appendChild(child);
-}
-
-//method 2
-
-for (var i = 0; i < 4; i++) {
- addChildren.appendChild(document.createElement('p'));
-}
-
-//method 3
-
-for (var i = 0; i < 4; i++) {
-  addChildren.innerHTML += "<p>Plop</p>";
+  $('#addChildren').append('<p>Yop</p>');
 }
